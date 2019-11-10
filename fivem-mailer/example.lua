70192f9d9c60dd831c59c90394fcbfa353608603
meta-hub <56391287+meta-hub@users.noreply.github.com>
2019-11-10 10:57:55 +1030

    Update example.lua

--- fivem-mailer/example.lua
+++ fivem-mailer/example.lua
@@ -2,10 +2,9 @@
 --  Credits to https://smtpjs.com/v3/smtp.js for the Email class.
 --
 
--- The data
-local target = 'targetEmail@provider.com'
-local subject = 'This is the email subject.'
-local htmlString = '<div style="background-color:rgba(55,55,55,1.0);border-radius:20px;"><h1><center>This is the email content</center><h1><p3><center>You can insert raw html here to style your email contents.</center><p3></div>'
+local targetEmail = 'targetEmail@provider.com'
+local testSubject = 'This is the email subject.'
+local testContent = '<div style="background-color:rgba(55,55,55,1.0);border-radius:20px;"><h1><center>This is the email content</center><h1><p3><center>You can insert raw html here to style your email contents.</center><p3></div>'
 
 -- From the client
 TriggerServerEvent('fivem-mailer:send',target,subject,htmlString)
